619efab9620afd11341f45ef87e2c79c67652315
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-06-16 16:06:52 +0300

    1

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = require("./config");
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose
+
 	.connect(DB_HOST)
 	.then(() => {
 		app.listen(3000, () => {
