84247ae511ba5b4642541d8b980464ccd0fbdef8
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-06-17 10:57:44 +0300

    1

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const { DB_HOST, PORT } = process.env;
 
+mongoose.set("strictQuery", true);
 mongoose
-
 	.connect(DB_HOST)
 	.then(() => {
 		app.listen(PORT, () => {
